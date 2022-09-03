9233891ec270e7ff76e745984ebbdc5448096d86
Leisan Iusupova <leisan_11_@mail.ru>
2022-09-03 23:20:57 +0300

    Ограничит количество форм за раз

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -2,6 +2,7 @@ import EventsListView from '../view/trip-events-list-view.js';
 import PointPresenter from './point-presenter.js';
 import NoPointView from '../view/no-point-view.js';
 import SortingView from '../view/sorting-view.js';
+import { updateItem } from '../util.js';
 import { destinations } from '../mock/destinations.js';
 import { offersByType } from '../mock/offers.js';
 import {render, RenderPosition} from '../framework/render.js';
@@ -15,6 +16,7 @@ export default class EventsPresenter {
   #noPointsComponent = new NoPointView();
   #wayPoints = [];
   #offers = [];
+  #pointPresenter = new Map();
 
   constructor(eventsContainer, wayPointsModel) {
     this.#eventsContainer = eventsContainer;
@@ -33,15 +35,31 @@ export default class EventsPresenter {
     render(this.#sortComponent, this.#eventsListComponent.element, RenderPosition.AFTERBEGIN);
   };
 
+  #handleModeChange = () => {
+    this.#pointPresenter.forEach((presenter) => presenter.resetView());
+  };
+
+  // #handlePointChange = (updatedPoint) => {
+  //   this.#wayPoints = updateItem(this.#wayPoints, updatedPoint);
+  //   this.#pointPresenter.get(updatedPoint.id).init(updatedPoint);
+  // };
+
 
   #renderPoint = (wayPoint) => {
-    const pointPresenter = new PointPresenter(this.#eventsListComponent.element);
+    const pointPresenter = new PointPresenter(this.#eventsListComponent.element, this.#handleModeChange);
 
     pointPresenter.init(wayPoint);
+
+    this.#pointPresenter.set(wayPoint.id, pointPresenter);
   }
 
   #renderNoPoints = () => {
     render(this.#noPointsComponent, this.#eventsListComponent.element);
+  };
+
+  #clearPointList = () => {
+    this.#pointPresenter.forEach((presenter) => presenter.destroy());
+    this.#pointPresenter.clear();
   };
 
   #renderPointsList = () => {
